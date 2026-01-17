48faff84ba834ee036bb1ad580ca1d298248c1cf
al-imran-sonet <alimransonet18@gmail.com>
2026-01-17 19:08:03 +0600

    Add additional infromation V1.2

--- Practice2_ch5.sql
+++ Practice2_ch5.sql
@@ -1,1 +1,12 @@
--- Group by Chapter 5
+-- Group by, Chapter 5
+
+select dept from employee 
+group by dept;
+
+select dept, count(id) from employee 
+group by dept;   -- It will count inside the group . 
+
+select dept, sum(salary) from employee 
+group by dept;
+
+
